458abc405eaa80b2d85e435af75e5742ef8b2c67
RolskyiStas <stas.rolskiy@gmail.com>
2023-02-02 17:28:35 +0200

    2.02.2023

---
+++ 8kuy/The'If'Function.js
@@ -0,0 +1,17 @@
+// The 'if' function
+
+function _if(bool, func1, func2) {
+  bool ? func1() : func2();
+}
+
+console.log(
+  _if(
+    true,
+    function () {
+      console.log("True");
+    },
+    function () {
+      console.log("false");
+    }
+  )
+);
